5a702311545d6ebd681e6dd7ac913f8f688b9946
JanVizcaino <jan.vizcaino@outlook.es>
2026-01-30 17:36:09 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CardSortingGame from './CardSortingGame';
+import './index.css'
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <CardSortingGame />
+    </div>
+  );
+}
+
+export default App;
